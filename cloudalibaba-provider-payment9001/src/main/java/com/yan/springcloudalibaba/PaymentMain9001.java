080ecc94e9b9ccad3da5e255fd0d812031ed2a44
yanrui <2221595915@qq.com>
2020-07-17 17:53:56 +0800

    nacos服务器提供者注册

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/yan/springcloudalibaba/PaymentMain9001.java
@@ -0,0 +1,20 @@
+package com.yan.springcloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: Ryan
+ * Date: 2020-07-17
+ * Time: 14:40
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class,args);
+    }
+}
